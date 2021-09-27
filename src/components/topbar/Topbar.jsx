66a981aa44a2f605caeb67dbab2a63dfbf626808
MohamedGouaouri <im_gouaouri@esi.dz>
2021-09-27 22:16:05 +0100

    finish navbar

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './topbar.css';
+import { NotificationsNone, Language, Settings} from '@material-ui/icons';
+export default function Topbar(){
+    return <div className="topbar">
+        <div className="topbarWrapper">
+            <div className="topLeft">
+                <h2>Dashboard</h2>
+            </div>
+            <div className="topRight">
+                <div className="notificationIcons">
+                    <NotificationsNone />
+                </div>
+                <div className="languageIcon">
+                    <Language />
+                </div>
+                <div className="settingsIcon">
+                    <Settings />
+                </div>
+                <img src="https://cdn.vox-cdn.com/thumbor/J2XSqgAqREtpkGAWa6rMhkHA1Y0=/0x0:1600x900/1400x933/filters:focal(672x322:928x578):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/66320060/Tanjiro__Demon_Slayer_.0.png" alt="" className="avatarTop"/>
+            </div>
+        </div>
+
+    </div>
+}
